e71e3897c54232060d69663263c4bcd2528bff77
Eduardo Würch <ewurch@gmail.com>
2024-02-05 23:51:24 +0100

    improve lsp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,4 +4,22 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "html",
+        "htmldjango",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
